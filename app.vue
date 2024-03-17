367a290e2f6686a9af8fed99702335648e4b464c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:09:08 +0400

    created app.vue

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { UserContextProvider } from "@nuxtbase/auth-ui-vue";
+const supabaseClient = useSupabaseClient();
+</script>
+<template>
+  <UserContextProvider :supabaseClient="supabaseClient">
+    <NuxtLayout>
+      <NuxtPage />
+      <ClientOnly>
+        <ModalsContainer />
+        <Toaster />
+      </ClientOnly>
+    </NuxtLayout>
+  </UserContextProvider>
+</template>
