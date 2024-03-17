84ee1d6e1023cb2737af70809ea83273981b1492
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:13:44 +0400

    created Modals container

---
+++ components/ModalsContainer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useModalsStore } from "@/stores/modals";
+
+const { type } = storeToRefs(useModalsStore());
+
+import AuthModal from "@/components/modals/AuthModal.vue";
+import UploadModal from "@/components/modals/UploadModal.vue";
+
+const computedComponent = computed(() => {
+  if (type.value !== null) {
+    switch (type.value) {
+      case "auth":
+        return AuthModal;
+      case "upload":
+        return UploadModal;
+    }
+  }
+
+  return null;
+});
+</script>
+
+<template>
+  <component :is="computedComponent" />
+</template>
