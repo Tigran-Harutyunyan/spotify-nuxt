62c4659e7f2369631e345958348846908e1a1080
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:13:26 +0400

    created PlayButton

---
+++ components/PlayButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import FaPlay from "./ui/icons/FaPlay.vue";
+</script>
+
+<template>
+  <button
+    class="transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md translate translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110"
+  >
+    <FaPlay class="text-black" />
+  </button>
+</template>
