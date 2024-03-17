6c0424e6c66a6aeea66599ec6330b596233becc8
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:12:19 +0400

    created Box component

---
+++ components/Box.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+interface BoxProps {
+  className?: string;
+}
+
+const { className } = defineProps<BoxProps>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        `
+        bg-neutral-900 
+        rounded-lg 
+        h-fit 
+        w-full
+        `,
+        className
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
