a949bfa5f7125d899a44038c6d0fdbede228bffa
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:15:01 +0400

    created Index page

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { type Song } from "@/types";
+import { useMainStore } from "@/stores/main";
+
+const { addSongCount } = storeToRefs(useMainStore());
+
+const { data: songs, refresh } = await useAsyncData<Song[]>("serverData", () =>
+  $fetch(`/api/songs`)
+);
+
+watch(
+  () => addSongCount.value,
+  () => {
+    refresh();
+  }
+);
+</script>
+
+<template>
+  <div
+    class="bg-neutral-900 md:rounded-lg h-full w-full overflow-hidden overflow-y-auto"
+  >
+    <Header>
+      <div class="mb-2">
+        <h1 class="text-white text-3xl font-semibold">Welcome back</h1>
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"
+        >
+          <ListItem
+            name="Liked Songs"
+            image="/images/liked.png"
+            href="/liked"
+          />
+        </div>
+      </div>
+    </Header>
+    <div class="mt-2 mb-7 px-6">
+      <div class="flex justify-between items-center">
+        <h1 class="text-white text-2xl font-semibold">Newest songs</h1>
+      </div>
+      <PageContent :songs="songs" v-if="songs" />
+    </div>
+  </div>
+</template>
