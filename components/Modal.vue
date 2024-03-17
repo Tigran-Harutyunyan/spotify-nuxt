9b00c7596992e5cf39833cb7b05b649a6222b727
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:13:12 +0400

    created Modal component

---
+++ components/Modal.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { DialogClose } from "radix-vue";
+import CloseDialogIcon from "./ui/icons/CloseDialogIcon.vue";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+
+const emit = defineEmits(["close"]);
+interface ModalProps {
+  show: boolean;
+  title: string;
+  description: string;
+}
+const { show, title, description } = withDefaults(defineProps<ModalProps>(), {
+  show: false,
+});
+</script>
+
+<template>
+  <Dialog v-model:open="show">
+    <DialogContent>
+      <DialogClose as-child>
+        <CloseDialogIcon @click="emit('close')"></CloseDialogIcon>
+      </DialogClose>
+
+      <DialogHeader class="pt-2 px-6">
+        <DialogTitle>
+          {{ title }}
+        </DialogTitle>
+        <DialogDescription>
+          {{ description }}
+        </DialogDescription>
+      </DialogHeader>
+
+      <slot />
+    </DialogContent>
+  </Dialog>
+</template>
