1043c358485805e31ac862591ab075eda33c1a0c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-17 12:12:36 +0400

    created authModal

---
+++ components/modals/AuthModal.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@nuxtbase/auth-ui-vue";
+import { useModalsStore } from "@/stores/modals";
+
+const supabaseClient = useSupabaseClient();
+
+const { type, isOpen } = storeToRefs(useModalsStore());
+
+const { onClose } = useModalsStore();
+
+const isModalOpen = isOpen.value && type.value === "auth";
+
+const { user } = useUser();
+
+watch(
+  () => user.value,
+  (newValue) => {
+    if (newValue) {
+      onClose();
+    }
+  }
+);
+</script>
+
+<template>
+  <Modal
+    title="Welcome back"
+    description="Login to your account."
+    :show="isModalOpen"
+    @close="onClose()"
+  >
+    <Auth
+      :supabaseClient="supabaseClient"
+      :providers="['google']"
+      theme="dark"
+      :magicLink="true"
+      :appearance="{
+        theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: '#404040',
+              brandAccent: '#22c55e',
+            },
+          },
+        },
+      }"
+    />
+  </Modal>
+</template>
