555c2c075d7ef12d517ed26b25d660d447243eeb
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-18 10:54:29 +0400

    added useLoadSongUrl composable

---
+++ composables/useLoadSongUrl.ts
@@ -0,0 +1,16 @@
+import { type Song } from "@/types";
+
+export const useLoadSongUrl = (song: Song) => {
+    const supabaseClient = useSupabaseClient();
+
+    if (!song) {
+        return '';
+    }
+
+    const { data: songData } = supabaseClient
+        .storage
+        .from('songs')
+        .getPublicUrl(song.song_path);
+
+    return songData.publicUrl;
+};
